329078abb64d6b33ebcd211e7afbf96a073f3d72
Avinash Nadig <avinash@hackerrank.com>
2020-08-03 04:32:07 -0500

    Added logger and made the bigList static

--- src/main/java/com/mastercard/challenge/controller/Parser.java
+++ src/main/java/com/mastercard/challenge/controller/Parser.java
@@ -7,12 +7,19 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * 
  * @author User
  *
  */
 public class Parser {
+	
+	private static List<List<String>> bigFileList = new ArrayList<>();
+	
+	private static final Logger log = LoggerFactory.getLogger(Parser.class);
 	
 	/**
 	 * Parses the given file and builds a list of list of cities interconnected by road.
@@ -21,30 +28,31 @@ public class Parser {
 	 */
 	public static List<List<String>> parseFile(){
 		
-		List<List<String>> bigFileList = new ArrayList<>();
-		
 		List<String> fileList = null;
 		
-		try {
-		      File myObj = new File("C:\\City.txt");
-		      Scanner myReader = new Scanner(myObj);
-		      while (myReader.hasNextLine()) {
-		    	fileList = new ArrayList<>(); 
-		        String data = myReader.nextLine();
-		        
-		        int commaIndex = data.indexOf(",");
-		        fileList.add(data.substring(0, commaIndex));
-		        fileList.add(data.substring(commaIndex +2, data.length()));
-		        Collections.sort(fileList);
-		        
-		        System.out.println(data);
-		        bigFileList.add(fileList);
-		      }
-		      myReader.close();
-	    } catch (FileNotFoundException e) {
-	      System.out.println("An error occurred.");
-	      e.printStackTrace();
-	    }
+		if (bigFileList.size()==0 ) {
+			try {
+				  log.info("Opening connection to File...");
+			      File myObj = new File("C:\\City.txt");
+			      Scanner myReader = new Scanner(myObj);
+			      while (myReader.hasNextLine()) {
+			    	fileList = new ArrayList<>(); 
+			        String data = myReader.nextLine();
+			        
+			        int commaIndex = data.indexOf(",");
+			        fileList.add(data.substring(0, commaIndex));
+			        fileList.add(data.substring(commaIndex +2, data.length()));
+			        Collections.sort(fileList);
+			        
+			        System.out.println(data);
+			        bigFileList.add(fileList);
+			      }
+			      myReader.close();
+		    } catch (FileNotFoundException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+		}
 		return bigFileList;
 	}
 }
